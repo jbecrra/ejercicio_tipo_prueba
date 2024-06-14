c04d479ba3cd2618b7059cc087de84c410735a37
johanbecerra <joh.becerram@duocuc.cl>
2024-06-14 11:24:20 -0400

    opcion 1 lista sin validaciones

--- clase_14-06.py
+++ clase_14-06.py
@@ -1,5 +1,5 @@
 import os, time
-
+trabajadores=[]
 while True:
     print("MENU DE TRABAJADORES")
     print("1. Registrar trabajador")
@@ -8,7 +8,16 @@ while True:
     print("4.  salir del programa")
     opc=int(input("ingrese opcion: "))
     if opc==1:
-        pass
+        print("REGISTRO TRABAJADOR")
+        nombre_apellido=input("ingrese su nombre y apellido: ")
+        cargo=int(input("ingrese cargo(1:CEO, 2:DESARROLLADOR, 3:ANALISTA): "))
+        sueldo_bruto=int(input("ingrese sueldo bruto: "))
+        desc_salud=int(7/100*sueldo_bruto)
+        desc_afp=int(12/100*sueldo_bruto)
+        sueldo_liquido=sueldo_bruto-desc_salud-desc_afp
+        trabajador=[nombre_apellido,cargo,sueldo_bruto,desc_salud,desc_afp,sueldo_liquido]
+        trabajadores.append(trabajador)
+        print("TRABAJADOR AGREGADO CON ÉXITO!")
     elif opc==2:
         pass
     elif opc==3:
