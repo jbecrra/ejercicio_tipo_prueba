27affa2bbc705749f98f87a1653a06832b1f5de9
johanbecerra <joh.becerram@duocuc.cl>
2024-06-14 11:05:19 -0400

    ejercicio solo con menu

---
+++ clase_14-06.py
@@ -0,0 +1,19 @@
+import os, time
+
+while True:
+    print("MENU DE TRABAJADORES")
+    print("1. Registrar trabajador")
+    print("2. Liatar todos los trabajadores")
+    print("3. Imprimir planilla de sueldos")
+    print("4.  salir del programa")
+    opc=int(input("ingrese opcion: "))
+    if opc==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc==3:
+        pass
+    else:
+        print("Gracias por usar el programa, adios")
+        break
+    time.sleep(3)
